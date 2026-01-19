60cd9c22457b8e20300d6c8799e78e1c41852fba
Robson Lopes Cavalcante <roblucci100@gmail.com>
2026-01-19 12:36:35 -0300

    update: middleware para proxy

--- presenca_ufc/src/proxy.ts
+++ presenca_ufc/src/proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse, type NextRequest } from 'next/server'
 const PUBLIC_ROUTES = ['/signin', '/signup']
 const PROTECTED_ROUTES = ['/dashboard']
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
   const hasToken = request.cookies.has('token-ufc')
   
